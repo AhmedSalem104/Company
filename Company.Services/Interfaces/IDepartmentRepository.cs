d29ddd0a4e429a33451ab4fcb0d22ffc423e767d
ahmed salem <AhmedSalem104>
2025-03-09 01:01:34 +0200

    Add Services For Department Entity

---
+++ Company.Services/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using Company.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Services.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+        Department? Get(int Id);
+        int Add(Department model);
+        int Update(Department model);
+        int Delete(Department model);
+    }
+}
