50542cdf0bb092889857a9babe71a099c7272fb4
ahmed salem <AhmedSalem104>
2025-03-09 02:03:30 +0200

    Create Department controller

--- Company.Data/Data/Contexts/CompanyDbContext.cs
+++ Company.Data/Data/Contexts/CompanyDbContext.cs
@@ -13,15 +13,15 @@ namespace Company.Data.Data.Contexts
     public class CompanyDbContext : DbContext
     {
 
-        public CompanyDbContext() : base() 
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options) 
         {
         
         }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server = DESKTOP-M87APL7\\SQLEXPRESS ; Database = CompanyFirstProject; Trusted_Connection = True ; TrustServerCertificate = True");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server = DESKTOP-M87APL7\\SQLEXPRESS ; Database = CompanyFirstProject; Trusted_Connection = True ; TrustServerCertificate = True");
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -1,3 +1,8 @@
+using Company.Data.Data.Contexts;
+using Company.Services.Interfaces;
+using Company.Services.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 namespace Company.Web
 {
     public class Program
@@ -8,6 +13,16 @@ namespace Company.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>(); // Allow DI For DepartmentRepository
+
+            // builder.Services.AddScoped<CompanyDbContext>(); // Allow DI For CompanyDbContext
+            //builder.Services.AddDbContext<CompanyDbContext>(); // Allow DI For CompanyDbContext
+
+
+           
+
+            builder.Services.AddDbContext<CompanyDbContext>(option => { option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); });
+                
 
             var app = builder.Build();
 
