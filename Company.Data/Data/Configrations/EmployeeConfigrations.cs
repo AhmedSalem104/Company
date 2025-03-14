0d61d0fd5b0e6e66b6201b188979b463763bbb2f
ahmed salem <AhmedSalem104>
2025-03-14 15:55:25 +0200

    update

--- Company.Data/Data/Configrations/EmployeeConfigrations.cs
+++ Company.Data/Data/Configrations/EmployeeConfigrations.cs
@@ -14,6 +14,7 @@ namespace Company.Data.Data.Configrations
         public void Configure(EntityTypeBuilder<Employee> builder)
         {
          
+
             builder.HasKey(e => e.Id);
 
             builder.Property(e => e.Id)
