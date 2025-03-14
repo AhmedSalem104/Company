ce92454af9cab936c5a06e870fd3ad27c819ba72
ahmed salem <AhmedSalem104>
2025-03-14 15:42:02 +0200

    Update

--- Company.Data/Models/Employee.cs
+++ Company.Data/Models/Employee.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Company.Data.Models
 {
-    public class Employee
+    public class Employee : BaseEntity
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
+      
+      
         public int? Age { get; set; }
         public string? Email { get; set; }
         public string? Address { get; set; }
