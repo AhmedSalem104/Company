00ff513a4032d27f1166b327fdcd30cde53bd4c8
ahmed salem <AhmedSalem104>
2025-03-21 14:28:10 +0200

    UpdateDesign

--- Company.Web/wwwroot/js/site.js
+++ Company.Web/wwwroot/js/site.js
@@ -1,10 +1,1 @@
-﻿let searchTimeout;
-document.getElementById("searchInput").addEventListener("keyup", function () {
-    clearTimeout(searchTimeout); 
-    searchTimeout = setTimeout(() => {
-        let query = this.value.trim(); 
-        if (query.length > 0) {
-            document.getElementById("searchForm").submit();
-        }
-    }, 500); 
-});
+﻿
