395e69892b39964fc243ce2783de9b5884c6ffa7
ahmed salem <AhmedSalem104>
2025-03-25 23:54:14 +0200

    Identity

---
+++ Company.Web/DTO/SignInDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.DTO
+{
+    public class SignInDTO
+    {
+      
+
+        [Required(ErrorMessage = "يرجى إدخال البريد الإلكتروني.")]
+        [EmailAddress(ErrorMessage = "البريد الإلكتروني الذي أدخلته غير صحيح.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "يرجى إدخال كلمة المرور.")]
+        [MinLength(6, ErrorMessage = "كلمة المرور يجب أن تكون 6 أحرف على الأقل.")]
+        [MaxLength(20, ErrorMessage = "كلمة المرور يجب أن تكون 20 حرفًا كحد أقصى.")]
+        public string? Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
